3bb5f9d043262cae4b078df629f8ae902fae1137
wonsunil <seonil6352@gmail.com>
2020-12-08 15:19:46 +0900

    Feat: DB java class file

---
+++ shopping/src/shopping/DB.java
@@ -0,0 +1,41 @@
+package shopping;
+
+import java.sql.*;
+
+public class DB {
+	public static Connection getDB() throws Exception {
+		Class.forName("oracle.jdbc.OracleDriver");
+        
+        return DriverManager.getConnection("jdbc:oracle:thin:@//localhost/xe", "center_jsp", "1234");
+	};
+	public static boolean execute(String query, String[] datas) {
+		Connection db = null;
+		
+		try {
+			db = getDB();
+			
+			PreparedStatement pstmt = db.prepareStatement(query);
+			
+			int index = 1;
+			for(String data : datas) {
+				pstmt.setString(index, data);
+				
+				index++;
+			}
+			
+			return pstmt.execute();
+		} catch (Exception ignored) {
+			return false;
+		} finally {
+			try {
+				db.close();
+			} catch (SQLException ignored) {}
+		}
+	};
+	public static ResultSet fetch(String query) throws Exception {
+		Connection db = getDB();
+		ResultSet rs = db.prepareStatement(query).executeQuery();
+				
+		return rs;
+	};
+}
